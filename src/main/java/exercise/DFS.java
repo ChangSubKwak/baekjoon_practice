56eb36d941146bf7a4694e8c55d7af8f0c41861b
changsubkwak <paradise1732@gmail.com>
2023-07-23 23:54:17 +0900

    feat: solved - Depth First Search

--- src/main/java/exercise/DFS.java
+++ src/main/java/exercise/DFS.java
@@ -1,10 +1,28 @@
 package exercise;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DFS {
 
-    public List<Integer> execute() {
-        return null;
+    private void dfs(int startNodeNum, List<List<Integer>> node, boolean[] visited, List<Integer> result) {
+        if (visited[startNodeNum]) {
+            return;
+        }
+
+        visited[startNodeNum] = true;
+        result.add(startNodeNum);
+
+        List<Integer> nodes = node.get(startNodeNum);
+        for (Integer nodeNum : nodes) {
+            dfs(nodeNum, node, visited, result);
+        }
+    }
+
+    public List<Integer> execute(List<List<Integer>> node) {
+        List<Integer> result = new ArrayList<>();
+        dfs(0, node, new boolean[node.size()], result);
+        System.out.println(result);
+        return result;
     }
 }
